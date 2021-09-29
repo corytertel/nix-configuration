497c81a0c60b939214e9a39095ee3b47fdb61ee8
corytertel <ctertel@comcast.net>
2021-09-29 14:45:18 -0700

    xmobar changes

--- shared/system/configuration.nix
+++ shared/system/configuration.nix
@@ -47,7 +47,8 @@
     enable = true;
     xkbModel = "microsoft";
     layout = "us,ru(winkeys)";
-    xkbOptions = "grp:toggle"; # ralt toggle keyboard
+    #xkbOptions = "grp:toggle"; # ralt toggle keyboard
+    xkbOptions = "grp:caps_toggle"; # caps toggle keyboard
     xkbVariant = "winkeys";
   };
 
