ba8a0f643606178fc6cb88e56eaa197af433f139
corytertel <ctertel@comcast.net>
2022-07-09 22:10:24 -0700

    fixed unneccesary color change in plasma theme

--- packages/oxygen-kde4-theme.nix
+++ packages/oxygen-kde4-theme.nix
@@ -9,7 +9,7 @@ self: super: {
       url = "https://github.com/corytertel/Oxygen-KDE-4.git";
       ref = "master";
       # rev = "9afe51f6ea66bbf9cf82b9d1a514d43e14c65f3f";
-      rev = "3ee704452778d4e3c94ce16c6fa86bf4a9fb103e";
+      rev = "72f36788a6c85502b989e7c2238be3422df44175";
     };
 
     installPhase = ''
