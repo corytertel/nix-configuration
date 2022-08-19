15d283fe1b150e99c363edaa7624ff98e0fe5b3d
corytertel <ctertel@comcast.net>
2022-08-18 17:05:12 -0700

    pkg update

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -2,10 +2,10 @@
 
 final: prev: let
   source = prev.discord.overrideAttrs (_: {
-    src = builtins.fetchTarball {
-      url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-      sha256 = "1kwqn1xr96kvrlbjd14m304g2finc5f5ljvnklg6fs5k4avrvmn4";
-    };
+    # src = builtins.fetchTarball {
+    #   url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+    #   sha256 = "1kwqn1xr96kvrlbjd14m304g2finc5f5ljvnklg6fs5k4avrvmn4";
+    # };
   });
 
   commandLineArgs = toString [
