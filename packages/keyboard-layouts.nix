01460cfc70faf45ae115da128ed84016382e11c4
corytertel <ctertel@comcast.net>
2022-09-02 20:47:28 -0700

    added ru_dvorak

--- packages/keyboard-layouts.nix
+++ packages/keyboard-layouts.nix
@@ -6,14 +6,13 @@ self: super: {
     dontBuild = true;
     installPhase = ''
       mkdir -p $out/share/X11/xkb/symbols
-      cp -a $src/v3/us_programmer $out/share/X11/xkb/symbols/us_programmer
-      cp -a $src/v3/ru_programmer $out/share/X11/xkb/symbols/ru_programmer
+      cp -a $src/v3/* $out/share/X11/xkb/symbols/
     '';
     src = pkgs.fetchFromGitHub {
       owner = "corytertel";
       repo = "keyboard-layouts";
-      rev = "1305e76c87d547bf92c98a94d6cdbcfe3ee26d2b";
-      sha256 = "foEqjX3Yimzf1Ove/OhP2duSlNCZV1rgR3o+WwAtIGk=";
+      rev = "d61689898bf04405108f535fb16503cd7186f37e";
+      sha256 = "xuwtQImu424yHjqkfnxgaCGnmt766gvl4aiHHUZOn6g=";
     };
   };
 }
