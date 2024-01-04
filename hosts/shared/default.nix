b2d9ed682d19ade7bb468ae1a9a71ca12d0d3a36
corytertel <ctertel@comcast.net>
2024-01-04 13:03:09 -0700

    Switch to pipewire for audio

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -84,7 +84,15 @@
   };
 
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # jack.enable = true; # to use JACK applications
+  };
 
   users = {
     users = {
